2aba7cca5a8e78d7fa867b2235da915a9fb3203f
mseif <mseif@sideitsolutions.com>
2021-08-26 21:05:25 -0300

    Deploy en heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './clases/usuario';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  unUsuario: Usuario;
+  
+  constructor() {
+    this.unUsuario= new Usuario();
+  }
+  saludar(){
+    console.log('Hola mundo');
+  }
 }
