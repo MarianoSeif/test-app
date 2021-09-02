d53211ef0741303da8d197de5b725efe2c61b3b6
mseif <mseif@sideitsolutions.com>
2021-09-02 14:53:01 -0300

    Componente quiensoy mejorado

--- src/app/components/quiensoy/quiensoy.component.ts
+++ src/app/components/quiensoy/quiensoy.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiensoy.component.css']
 })
 export class QuiensoyComponent implements OnInit {
+  data: Object = {};
+  userName: string = '';
+  id: string = '';
+  repos: number = 0;
+  avatarUrl:string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    fetch('https://api.github.com/users/MarianoSeif')
+    .then(response => response.json())
+    .then(response => {
+      //console.log(response);
+      this.userName = response.login;
+      this.id = response.id;
+      this.repos = response.public_repos;
+      this.avatarUrl = response.avatar_url;
+    })
   }
 
 }
