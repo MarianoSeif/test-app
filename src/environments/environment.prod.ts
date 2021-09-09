60328f407ed7cec4562534772fb7a747d138a6ea
mseif <mseif@sideitsolutions.com>
2021-09-09 18:43:01 -0300

    Agrego variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwQMEymWStcplx1iO0tYts0lcx736u53w",
+    authDomain: "test-app-c447c.firebaseapp.com",
+    projectId: "test-app-c447c",
+    databaseURL: "https://test-app-c447c-default-rtdb.firebaseio.com/",
+    storageBucket: "test-app-c447c.appspot.com",
+    messagingSenderId: "774247988314",
+    appId: "1:774247988314:web:9028a4ca2a30314d4fec63",
+    measurementId: "G-8YT4HQ9N7G"
+  }
 };
